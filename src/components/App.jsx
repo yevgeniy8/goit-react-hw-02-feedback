02243a8ba06d482091aefc3e2c4e3943772c58df
Yevgeniy Utkin <yevgeniy.utkin@gmail.com>
2023-06-11 23:56:14 +0300

    commit 1

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,18 @@
+import Feedback from './Feedback/Feedback';
+
 export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+    return (
+        <div
+        // style={{
+        //     height: '100vh',
+        //     display: 'flex',
+        //     justifyContent: 'center',
+        //     alignItems: 'center',
+        //     fontSize: 40,
+        //     color: '#010101',
+        // }}
+        >
+            <Feedback options={['good', 'neutral', 'bad']} />
+        </div>
+    );
 };
